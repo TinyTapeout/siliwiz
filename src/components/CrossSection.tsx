10acda4465f48d79e1af2c859cc9ffb755ca38a2
Uri Shaked <uri@urishaked.com>
2022-10-30 19:15:16 +0200

    fix(xsection): use same scale as canvas
    fix #1

--- src/components/CrossSection.tsx
+++ src/components/CrossSection.tsx
@@ -25,7 +25,7 @@ export default function CrossSection(props: { height: number }) {
           }
         />
       </span>
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200" width="200" height="200">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 400 400" width="400" height="400">
         <defs>
           <pattern
             id="hatch-pattern"
