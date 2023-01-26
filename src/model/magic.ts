17fe158f5f1d164ce7604bccba978ff57cd5d192
Uri Shaked <uri@urishaked.com>
2023-01-26 15:43:36 +0200

    fix(magic): wrong layer for labels
    all labels were hardcoded for `metal1, even if set on a different layer.

--- src/model/magic.ts
+++ src/model/magic.ts
@@ -72,23 +72,25 @@ export function toMagic(layout: ILayout, tech = defaultTech) {
     `magscale 1 ${tech === 'sky130A' ? 2 : 1}`,
     `timestamp ${Math.floor(new Date().getTime() / 1000)}`,
   ];
+  const labels: { layerName: string; rect: ILayoutRect }[] = [];
   for (const layer of layerTypes) {
     const rects = layout.rects.filter((r) => r.layer === layer.name);
     if (rects.length) {
       result.push(`<< ${layer.magicName} >>`);
       for (const rect of rects) {
         result.push(`rect ${magicRect(rect)}`);
+        if (rect.label) {
+          labels.push({ layerName: layer.magicName, rect });
+        }
       }
     }
   }
   result.push('<< labels >>');
   let portIndex = 1;
-  for (const rect of layout.rects) {
-    if (rect.label) {
-      result.push(`flabel metal1 s ${magicRect(rect)} 0 FreeSans 240 90 0 0 ${rect.label}`);
-      result.push(`port ${portIndex} nsew signal output`);
-      portIndex++;
-    }
+  for (const { layerName, rect } of labels) {
+    result.push(`flabel ${layerName} s ${magicRect(rect)} 0 FreeSans 240 90 0 0 ${rect.label}`);
+    result.push(`port ${portIndex} nsew signal output`);
+    portIndex++;
   }
   result.push('<< end >>');
 
