fc976c178c5b299b9af25d595fd6f1f2e37c502d
Uri Shaked <uri@urishaked.com>
2023-01-31 17:30:54 +0200

    chore: run prettier on .css and .tsx files too

--- src/entry-client.tsx
+++ src/entry-client.tsx
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: Apache-2.0
 
-import { mount, StartClient } from "solid-start/entry-client";
+import { mount, StartClient } from 'solid-start/entry-client';
 
 mount(() => <StartClient />, document);
--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -1,11 +1,5 @@
 // SPDX-License-Identifier: Apache-2.0
 
-import {
-  createHandler,
-  renderAsync,
-  StartServer,
-} from "solid-start/entry-server";
+import { createHandler, renderAsync, StartServer } from 'solid-start/entry-server';
 
-export default createHandler(
-  renderAsync((event) => <StartServer event={event} />)
-);
+export default createHandler(renderAsync((event) => <StartServer event={event} />));
