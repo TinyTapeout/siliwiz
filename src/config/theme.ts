313e69ad6b2b1f048ab1c429ea689e13573adf5b
Uri Shaked <uri@urishaked.com>
2023-02-01 20:24:30 +0200

    fix: update theme color to match the new logo

--- src/config/theme.ts
+++ src/config/theme.ts
@@ -5,7 +5,7 @@ import { createTheme } from '@suid/material';
 export const theme = createTheme({
   palette: {
     primary: {
-      main: '#5b1da4',
+      main: '#544ead',
     },
     secondary: {
       main: '#8afbfd',
