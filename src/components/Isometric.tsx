88a16e72486353589dd2a8e6668a83e2071aab11
Marcus Alagar <mvala079@gmail.com>
2025-09-21 01:56:23 -0700

    feat: experimental 3D view
    Implement 3D viewer of current design in new
    "3D" tab by using `solid-three`, specifically
    its `next` branch.
    
    Used `Entity` pattern as opposed to `createT`
    pattern mainly as personal preference.
    
    Also added lighting using (admittedly) magic
    numbers to give visual contrast to 3D. Also
    used magic numbers to get in frame on own
    machine.

---
+++ src/components/Isometric.tsx
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: Apache-2.0
+
+import { For } from 'solid-js';
+import { Canvas, Entity } from 'solid-three';
+import * as THREE from 'three';
+import { layout, rectLayer, rectViaLayer } from '../model/layout';
+
+export default function Isometric() {
+  const rects = () => layout.rects;
+
+  return (
+    <Canvas defaultCamera={{ position: [0, 0, 900] }} shadows>
+      <Entity
+        from={THREE.Group}
+        position={[-275, 100, 0]}
+        rotation={[(7 * Math.PI) / 4, 0, Math.PI / 4]}
+      >
+        <For each={rects()}>
+          {(rect) => {
+            const layer = rectLayer(rect);
+            const viaLayer = rectViaLayer(layout, rect);
+            if (!viaLayer || !layer) {
+              return <Entity from={THREE.Mesh} />;
+            }
+
+            return (
+              <Entity
+                from={THREE.Mesh}
+                position={[
+                  rect.x + rect.width / 2,
+                  -(rect.y + rect.height / 2),
+                  -viaLayer.crossY - viaLayer.crossHeight / 2,
+                ]}
+              >
+                <Entity
+                  from={THREE.BoxGeometry}
+                  args={[rect.width, rect.height, viaLayer.crossHeight]}
+                />
+                <Entity from={THREE.MeshStandardMaterial} color={layer.color} />
+              </Entity>
+            );
+          }}
+        </For>
+      </Entity>
+      <Entity from={THREE.AmbientLight} intensity={0.1} />
+      <Entity from={THREE.PointLight} position={[0, 600, -250]} />
+    </Canvas>
+  );
+}
